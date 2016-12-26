e90d9d2d1844d501b94049351f99d1c51b318104
hegdeashwin <ashwin.hegde3@gmail.com>
2016-12-27 01:15:34 +0530

    Add accordion component + Release 0.0.1

---
+++ src/GSGAccordionSession.jsx
@@ -0,0 +1,52 @@
+import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import classNames from 'classnames';
+import objectAssign from 'object-assign';
+
+export default class GSGAccordionSession extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleCollapse = this.toggleCollapse.bind(this);
+  }
+
+  toggleCollapse() {
+    const _accordionDOM = ReactDOM.findDOMNode(this);
+    const _panelCollapse = _accordionDOM.querySelector('.panel-collapse');
+    _panelCollapse.classList.toggle('in');
+  }
+
+  render() {
+    const { _item, _bStyle } = this.props;
+
+    let _bClass = {
+      'panel': true
+    };
+
+    _bClass = objectAssign(_bClass, {
+      [`panel-${_bStyle}`]: true
+    });
+
+    _bClass = classNames(_bClass);
+
+    return(
+      <div className={_bClass}>
+        <div className="panel-heading" onClick={this.toggleCollapse}>
+          <h4 className="panel-title">
+            <a data-toggle="collapse">{_item.props.title}</a>
+          </h4>
+        </div>
+        <div className="panel-collapse collapse">
+          <div className="panel-body">
+            {_item.props.children}
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+GSGAccordionSession.propTypes = {
+  item: PropTypes.object,
+  bStyle: PropTypes.string
+};
